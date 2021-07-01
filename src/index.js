821746324db9ffed4ca3e2687a8f67465688e5b8
Eduards Leimanis <blademan82@gmail.com>
2021-07-01 17:59:58 +0300

    add cart

--- src/index.js
+++ src/index.js
@@ -2,13 +2,58 @@
 
 //checkbox
 
-let checkbox = document.querySelector("#discount-checkbox");
+let checkbox = document.querySelectorAll(".filter-check_checkbox");
 
-checkbox.addEventListener("change", function () {
-  if (this.checked) {
-    this.nextElementSibling.classList.add("checked");
-  } else {
-    this.nextElementSibling.classList.remove("checked");
-  }
+checkbox.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      this.nextElementSibling.classList.add("checked");
+    } else {
+      this.nextElementSibling.classList.remove("checked");
+    }
+  });
 });
+
 //end checkbox
+
+//cart
+let btnCart = document.querySelector("#cart");
+let modalCart = document.querySelector(".cart");
+let cartClose = document.querySelector(".cart-close");
+
+btnCart.addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+  modalCart.style.display = "flex";
+});
+
+cartClose.addEventListener("click", function () {
+  modalCart.style.display = "none";
+  document.body.style.overflow = "";
+});
+//End cart
+
+// Cart Items
+let cards = document.querySelectorAll(".goods  .card");
+let cardWraper = document.querySelector(".cart-wrapper");
+let cardEmpty = document.querySelector("#cart-empty");
+let counter = document.querySelector(".counter");
+
+cards.forEach((item) => {
+  const btn = item.querySelector("button");
+
+  btn.addEventListener("click", () => {
+    let cardClone = item.cloneNode(true);
+    cardWraper.append(cardClone);
+    cardEmpty.remove();
+    showData();
+  });
+});
+
+function showData() {
+  let counterITEM = 0;
+  let cardCarts = cardWraper.querySelectorAll(".card");
+  counterITEM = cardCarts.length;
+
+  counter.textContent = counterITEM;
+}
+//Cart Items END
