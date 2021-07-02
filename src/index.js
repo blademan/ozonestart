807bd93fd6728b47e7f4614a1b0e8f6d66f24edd
Eduards Leimanis <blademan82@gmail.com>
2021-07-03 01:44:47 +0300

    add script moduls

--- src/index.js
+++ src/index.js
@@ -1,82 +1,94 @@
 "use strict";
 
 //checkbox
+function toggleCheckbox() {
+  let checkbox = document.querySelectorAll(".filter-check_checkbox");
 
-let checkbox = document.querySelectorAll(".filter-check_checkbox");
-
-checkbox.forEach((checkbox) => {
-  checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      this.nextElementSibling.classList.add("checked");
-    } else {
-      this.nextElementSibling.classList.remove("checked");
-    }
+  checkbox.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      if (this.checked) {
+        this.nextElementSibling.classList.add("checked");
+      } else {
+        this.nextElementSibling.classList.remove("checked");
+      }
+    });
   });
-});
-
+}
+toggleCheckbox();
 //end checkbox
 
-//cart
-let btnCart = document.querySelector("#cart");
-let modalCart = document.querySelector(".cart");
-let cartClose = document.querySelector(".cart-close");
+//card
+function toggleCart() {
+  let btnCart = document.querySelector("#cart");
+  let modalCart = document.querySelector(".cart");
+  let cartClose = document.querySelector(".cart-close");
 
-btnCart.addEventListener("click", function () {
-  document.body.style.overflow = "hidden";
-  modalCart.style.display = "flex";
-});
+  btnCart.addEventListener("click", function () {
+    document.body.style.overflow = "hidden";
+    modalCart.style.display = "flex";
+  });
 
-cartClose.addEventListener("click", function () {
-  modalCart.style.display = "none";
-  document.body.style.overflow = "";
-});
+  cartClose.addEventListener("click", function () {
+    modalCart.style.display = "none";
+    document.body.style.overflow = "";
+  });
+}
+
+toggleCart();
+
 //End cart
 
 // Cart Items
-let cards = document.querySelectorAll(".goods  .card");
-let cardWraper = document.querySelector(".cart-wrapper");
-let cardEmpty = document.querySelector("#cart-empty");
-let counter = document.querySelector(".counter");
+function addCardItem() {
+  let cards = document.querySelectorAll(".goods  .card");
+  let cardWraper = document.querySelector(".cart-wrapper");
+  let cardEmpty = document.querySelector("#cart-empty");
+  let counter = document.querySelector(".counter");
 
-cards.forEach((item) => {
-  const btn = item.querySelector("button");
+  cards.forEach((item) => {
+    const btn = item.querySelector("button");
 
-  btn.addEventListener("click", () => {
-    let cardClone = item.cloneNode(true);
-    cardWraper.appendChild(cardClone);
+    btn.addEventListener("click", () => {
+      let cardClone = item.cloneNode(true);
+      cardWraper.appendChild(cardClone);
 
-    showData();
-    showPrice();
-
-    const removeBtn = cardClone.querySelector(".btn");
-    removeBtn.textContent = "Delete Item";
-    removeBtn.addEventListener("click", () => {
-      cardClone.remove();
       showData();
       showPrice();
+
+      const removeBtn = cardClone.querySelector(".btn");
+      removeBtn.textContent = "Delete Item";
+      removeBtn.addEventListener("click", () => {
+        cardClone.remove();
+        showData();
+        showPrice();
+      });
     });
   });
-});
 
-function showData() {
-  let cardCarts = cardWraper.querySelectorAll(".card");
-  counter.textContent = cardCarts.length;
+  function showData() {
+    let cardCarts = cardWraper.querySelectorAll(".card");
+    counter.textContent = cardCarts.length;
 
-  if (cardCarts.length !== 0) {
-    cardEmpty.remove();
-  } else {
-    cardWraper.appendChild(cardEmpty);
+    if (cardCarts.length !== 0) {
+      cardEmpty.remove();
+    } else {
+      cardWraper.appendChild(cardEmpty);
+    }
+  }
+
+  function showPrice() {
+    let priceSum = cardWraper.querySelectorAll(".card-price");
+    let cartTotal = document.querySelector(".cart-total span");
+    let sum = 0;
+
+    priceSum.forEach((price) => {
+      sum += parseFloat(price.innerHTML);
+    });
+    cartTotal.textContent = sum;
   }
 }
 
-function showPrice() {
-  let priceSum = cardWraper.querySelectorAll(".card-price");
-  let cartTotal = document.querySelector(".cart-total span");
-  let sum = 0;
-
-  priceSum.forEach((price) => {
-    sum += parseFloat(price.innerHTML);
-  });
-  cartTotal.textContent = sum;
-}
+addCardItem();
 //Cart Items END
+
+// Sale Filtrs
