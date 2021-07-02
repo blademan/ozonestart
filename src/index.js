8507660d9376bdbfa98ed92523d8bf62f9ae06ed
Eduards Leimanis <blademan82@gmail.com>
2021-07-03 01:31:03 +0300

    add delete item function

--- src/index.js
+++ src/index.js
@@ -43,14 +43,40 @@ cards.forEach((item) => {
 
   btn.addEventListener("click", () => {
     let cardClone = item.cloneNode(true);
-    cardWraper.append(cardClone);
-    cardEmpty.remove();
+    cardWraper.appendChild(cardClone);
+
     showData();
+    showPrice();
+
+    const removeBtn = cardClone.querySelector(".btn");
+    removeBtn.textContent = "Delete Item";
+    removeBtn.addEventListener("click", () => {
+      cardClone.remove();
+      showData();
+      showPrice();
+    });
   });
 });
 
 function showData() {
   let cardCarts = cardWraper.querySelectorAll(".card");
   counter.textContent = cardCarts.length;
+
+  if (cardCarts.length !== 0) {
+    cardEmpty.remove();
+  } else {
+    cardWraper.appendChild(cardEmpty);
+  }
+}
+
+function showPrice() {
+  let priceSum = cardWraper.querySelectorAll(".card-price");
+  let cartTotal = document.querySelector(".cart-total span");
+  let sum = 0;
+
+  priceSum.forEach((price) => {
+    sum += parseFloat(price.innerHTML);
+  });
+  cartTotal.textContent = sum;
 }
 //Cart Items END
